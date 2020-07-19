1cd2f8c9329cf9000bceff594ff130c54f6698a3
donovan1212 <32581031+donovan1212@users.noreply.github.com>
2020-07-18 22:38:44 -0500

    restructuring

---
+++ TestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Py_embedded_v37;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PythonAbstractions Python_Functions = new PythonAbstractions();
+            Python_Functions.RunScript();
+            Python_Functions.RunFunction(ScriptName: "ConfigLib", FuncName: "main");
+        }
+    }
+}
